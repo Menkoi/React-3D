f2e8d585c03c28970260e8bea4f2dae069ad7522
celaeon <celaeon@gmail.com>
2022-04-21 16:33:44 +0200

    changed camera position

--- src/threeTest.js
+++ src/threeTest.js
@@ -1,6 +1,6 @@
 import React, {Suspense, useRef} from "react";
 import {Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, Box, Stars} from "@react-three/drei";
+import { OrbitControls, Stars } from "@react-three/drei";
 import SpaceScene from './components/SpacePod';
 //import SpaceBackground from './components/SpaceBackground';
 
@@ -8,8 +8,8 @@ import SpaceScene from './components/SpacePod';
 function StarSpin() {
     const scene = useRef();
     useFrame(() => {
-        scene.current.rotation.y += 0.002;
-        //scene.current.rotation.x += 0.002;
+        scene.current.rotation.y += 0.0005;
+        scene.current.rotation.x += 0.0002;
         //scene.current.rotation.z += 0.04;
     });
     return (
@@ -23,15 +23,11 @@ function StarSpin() {
  export default function ThreeTest() {
     return (
       <div className="MainTest">
-        <h1>Lorem Ipsum</h1>
-        <Canvas camera={{ position: [-10, 15, 15], fov: 50 }}>
+        <Canvas camera={{ position: [-10, 10, -30], fov: 70}}>
           <color attach="background" args={['#161c24']}/>
-          <ambientLight intensity={1} />
-          <pointLight position={[10, 10, 10]} />
+          <ambientLight intensity={0.01} />
+          <pointLight position={[-10, 10, 10]} />
           <Suspense fallback={null}>
-            <Box>
-            <meshPhongMaterial attach="material" color="red"></meshPhongMaterial>
-            </Box>
             <SpaceScene></SpaceScene>
             <StarSpin></StarSpin>
           </Suspense>
